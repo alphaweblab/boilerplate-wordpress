494d7c536f68376e13927abfaa1c43c018e5a212
Hameed Rahamathullah <hameedraha@gmail.com>
2016-10-26 19:45:11 +0530

    Minor patches in Gulp process

--- build/header.php
+++ build/header.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 <head>
 	<meta charset="<?php bloginfo('charset'); ?>" />
-	<title><?php wp_title(); ?> | <?php bloginfo('name'); ?></title>
+	<title><?php wp_title(''); ?> &ndash; <?php bloginfo('name'); ?></title>
 	<meta name="description" content="<?php bloginfo('description'); ?>" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
